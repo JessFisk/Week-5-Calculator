cfd98574a7c7fdac6e51fd946b8b21478ac48693
Jessica Fisk <fisk.jess@gmail.com>
2023-02-10 16:56:05 +0000

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { evaluate } from 'mathjs';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const button = [
+    "Clear", "^", "%",
+    "(", ")", "-+", "/",
+    7, 8, 9, "*",
+    4, 5, 6, "-",
+    1, 2, 3, "+",
+    0, ".", "="
+  ];
+
+
+  const [calc, setCalc] = useState("")
+
+  const clickNumbers = (value) => {
+    setCalc(calc + value)
+  }
+  const clearCalc = () => {
+    setCalc("")
+  }
+  const equalsBtn = () => {
+    setCalc(evaluate(calc).toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header id="header">
+          <h1>Calculator</h1>
+        </header>
+        <div id="CalcBody">
+          <div id="buttonBox">
+            <div id="screen">{calc}</div>
+            {button.map((btn, index) => {
+              if (btn === "*" || btn === "-" || btn === "+" || btn === "/") {
+                return <button key={index} className="button functionBtn" onClick={() => clickNumbers(btn)}>{btn}</button>
+              }
+              if (btn === "=") {
+                return <button key={index} className="button" id="equals" onClick={() => equalsBtn()}>{btn}</button>
+              }
+              if (btn === "Clear") {
+                return <button key={index} className="button" id="clear" onClick={() => clearCalc()}>{btn}</button>
+              }
+              else {
+                return <button key={index} className="button" onClick={() => clickNumbers(btn)}>{btn}</button>
+              }
+            }
+            )}
+          </div>
+        </div>
+      </div>
   );
 }
 
